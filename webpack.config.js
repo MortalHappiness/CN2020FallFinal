95bcb12b0ea98d6562938dab4cb6a4afa59ac011
MortalHappiness <b07901069@ntu.edu.tw>
2020-12-03 17:46:03 +0800

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./client/src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: { extensions: [".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "build/"),
+    publicPath: "/build/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "client/public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/build/",
+    hotOnly: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
